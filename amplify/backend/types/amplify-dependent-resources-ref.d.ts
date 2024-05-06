b1d33f9d309f2169bbfbd955a8d9a974b670286d
Daniele Silva <daniele.silva@ifce.edu.br>
2024-05-06 19:07:45 -0300

    correcoes dados de imoveis e telefone

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "brandimoveis": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
